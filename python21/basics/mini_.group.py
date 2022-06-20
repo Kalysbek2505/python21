f3122ff2777ccf3a58164fcbaec6ef728a1784f6
Kalys2505 <zarypbekov.kalys@gmail.com>
2022-06-20 12:43:53 +0600

    tryexcept

--- python21/basics/mini_.group.py
+++ python21/basics/mini_.group.py
@@ -9,16 +9,16 @@
 #         continue
 
 
-import random   
-NumberToGuess=random.randint(1,100)   
-userGuess=-1        
-while True:      
-    userGuess=int(input("Угадай число от 1 до 100 "))      
-    if userGuess > NumberToGuess:        
-        print("Число должно быть меньше!")      
-    elif userGuess < NumberToGuess:         
-        print("Число должно быть больше!")      
-    else:         
-        print("Вы угадали, это число = " + str(NumberToGuess))           
+# import random   
+# NumberToGuess=random.randint(1,100)   
+# userGuess=-1        
+# while True:      
+#     userGuess=int(input("Угадай число от 1 до 100 "))      
+#     if userGuess > NumberToGuess:        
+#         print("Число должно быть меньше!")      
+#     elif userGuess < NumberToGuess:         
+#         print("Число должно быть больше!")      
+#     else:         
+#         print("Вы угадали, это число = " + str(NumberToGuess))           
                    
-        break
+#         break
